78a66aa4fcf17f35a28314d4dfd18eb98f8f0562
EduardaChagas <etcc@ic.ufal.br>
2020-08-19 17:51:03 -0300

    refresh

--- Code/Gabor.R
+++ Code/Gabor.R
@@ -138,7 +138,7 @@ gabor.analysis <- function(){
     cat("Munich ", j, "\n")
   }
   #Pasture
-  sar_data = raster(paste("../../Data/", "guatemala", "/HHHH", ".grd", sep = ""))
+  sar_data = raster(paste("../../Data/", "Guatemala", "/HHHH", ".grd", sep = ""))
   for(j in c(1:ns.pasture)){
     img = getValuesBlock(sar_data, row = dimen.pasture[j,1], nrows = dimen.pasture[j,2], col = dimen.pasture[j,3], ncols = dimen.pasture[j,4], format = "matrix")
     gb_f = init_gb$gabor_feature_extraction(image = img, scales = 5, orientations = 8, gabor_rows = 40,
